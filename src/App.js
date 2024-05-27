066a64d883768f4c443e6b9b668180f42d045bb7
Personal <personal@Personals-MacBook-Pro.local>
2024-05-27 23:28:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import * as React from 'react'
-import { useGetTodosQuery } from './services/todos'
-
-import PaginationTable from './ui/PaginationTable';
-import ImageSlider from './ui/ImageSlider';
 
 export default function App() {
-  const { data, error, isLoading } = useGetTodosQuery();
-  console.log("data", data);
-
   return (
     <div className="App">
-      {error ? (
-        <>Error</>
-      ) : isLoading ? (
-        <>Loading...</>
-      ) : data ? (
-        <PaginationTable data={data}/>
-      ) : null}
-
-<ImageSlider />
+     My test app 1
     </div>
     
   )
--- src/services/todos.js
+++ src/services/todos.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 // Define a service using a base URL and expected endpoints
 export const todosApi = createApi({
-  reducerPath: 'pokemonApi',
+  reducerPath: 'testApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://dummyjson.com' }),
   endpoints: (builder) => ({
     getTodos: builder.query({
